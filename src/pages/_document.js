e1c98c3187ad5b8893cdd73d663b95039a28acdd
puemmth <puem.mth@gmail.com>
2023-06-09 22:14:49 +0700

    head 2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,10 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-
-      <head>
-        <title>TimeTable</title>
-      </head>
       <body>
         <Main />
         <NextScript />
