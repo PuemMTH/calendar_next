0a4223fdd6e705fc3093b8300b600025c8c360b5
puemmth <puem.mth@gmail.com>
2023-06-09 22:14:05 +0700

    head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,9 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Template</title>
-      </Head>
+      <Head />
+
+      <head>
+        <title>TimeTable</title>
+      </head>
       <body>
         <Main />
         <NextScript />
